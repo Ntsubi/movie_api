1bc281b4004969f857ed16f9e73e0ad928423ee4
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-11-17 10:41:58 +0100

    Added Express get routes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+
+app.get('/movies', (req, res) => {
+    res.json('My top 10 movies');
+});
+
+app.get('/', (req, res) => {
+    res.send('Catch all your favourite movies on demand!');
+});
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080');
+});
