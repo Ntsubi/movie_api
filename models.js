e0a29d90badf62e4a3de46a0882aa2838bec02d2
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-11-27 10:35:08 +0100

    Defined movies collection schema

--- models.js
+++ models.js
@@ -1,2 +1,21 @@
 const mongoose = require('mongoose');
 
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String 
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: Date,
+        Death: Date
+    },
+    ImageURL: String,
+    Featured: Boolean,
+    Runtime: String,
+    ReleaseYear: number
+});
+
