e35ae6e36d9ed50193f18a60aa42b3a354f46333
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-11-29 12:21:49 +0100

    Configured Passport strategies

---
+++ passport.js
@@ -0,0 +1,49 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy, //This variable defines basic HTTP requests for login requests
+    Models = require('/models.js'), //Username must be checked within the database to verify user exists; password checked elsewhere
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy, //Enables authentication of users based on JWT submitted with requests
+    ExtractJWT = passport.JWT.ExtractJwt; //JWT is extracted from the header of HTTP request
+
+passport.use(
+    new LocalStrategy(
+        { usernameField: 'Username', passwordField: 'Password' },
+        async (username, password, callback) => {
+            console.log(`${username} ${password}`);
+            await Users.findOne({ Username: username })
+                .then((user) => {
+                    if (!user) {
+                        console.log('Incorrect username');
+                        return callback(null, false, {
+                            message: 'Incorrect username or password.',
+                        });
+                    }
+                    console.log('Finished');
+                    return callback(null, user);
+                })
+                .catch((error) => {
+                    if (error) {
+                        console.error(error);
+                        return callback(error);
+                    }
+                })
+        }
+    )
+);
+
+passport.use(new JWTStrategy (
+    {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+        secretOrKey: 'your_jwt_secret'
+    }, async (jwtPayload, callback) => {
+        return await Users.findById(jwtPayload._id)
+            .then((user) => {
+                return callback(null, user);
+            })
+            .catch((error) => {
+                return callback(error)
+            });
+    }));
+
