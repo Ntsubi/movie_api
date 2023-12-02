709425ab24785ce40c60855d2d4bbb89f1fe2508
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-02 15:44:34 +0100

    Implemented domain restrictions to API

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080'];
+
+app.use(cors( {
+    origin: (origin, callback) => {
+        if (!origin) return callback (null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback (new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth')(app); //(app) ensures that Express is available in the auth.js file as well
 const passport = require('passport');
     require('./passport');
