d15c116b3df7630510103d06f309187b6ecea561
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-02 17:30:03 +0100

    Edited mongoose.connect url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { check, validationResult } = require('express-validator'),
     Movies = Models.Movie;
     Users = Models.User;
 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://0.0.0.0:27017/moviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(bodyParser.json());
