2dd72e1069cd8a79afaa1eba4b5263e4f8ad6714
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-15 09:24:30 +0100

    Reenabled passport authentication

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 //Routes to the movies URL and returns the entire list of movies in the array/database
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
